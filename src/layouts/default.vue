f100e2a3486387b733555b6b3310d90f94da9e82
Arm <69001162+arskiir@users.noreply.github.com>
2021-11-05 18:14:26 +0700

    📱 for 320px screen

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-full bg-dark-700 text-white px-20 pt-10">
+  <div class="min-h-full bg-dark-700 text-white px-5 pt-5 sm:(px-20) md:(pt-10) ">
     <router-view />
   </div>
 </template>
