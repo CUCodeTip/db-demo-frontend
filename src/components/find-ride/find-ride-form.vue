0a70339cd7c56490fa2637e44ea376970fbb01da
Arm <69001162+arskiir@users.noreply.github.com>
2021-11-14 00:15:15 +0700

    ♻️ refactor the entire page into 2 vue components

---
+++ src/components/find-ride/find-ride-form.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { useFindRideStore } from '~/stores/findRideStore'
+const inputs = useFindRideStore()
+</script>
+
+<template>
+  <section class="space-y-4">
+    <div class="flex gap-15">
+      <div class="ipt-container">
+        <span class="text-xl">Start Location</span>
+        <input
+          v-model="inputs.startLocation"
+          class="ipt"
+          placeholder="Location"
+        >
+      </div>
+      <div class="ipt-container">
+        <span class="text-xl">Destination Location</span>
+        <input
+          v-model="inputs.destinationLocation"
+          class="ipt"
+          placeholder="Location"
+        >
+      </div>
+      <div class="ipt-container">
+        <span class="text-xl">Companions</span>
+        <input
+          v-model="inputs.companions"
+          class="ipt"
+          type="number"
+          min="0"
+        >
+      </div>
+    </div>
+    <div class="flex gap-15 items-end">
+      <div class="ipt-container">
+        <span class="text-xl">Start Date</span>
+        <input
+          v-model="inputs.startDate"
+          class="ipt"
+          type="date"
+        >
+      </div>
+      <div class="ipt-container">
+        <span class="text-xl">End Date</span>
+        <input
+          v-model="inputs.endDate"
+          class="ipt"
+          type="date"
+        >
+      </div>
+      <div class="flex gap-5">
+        <button
+          class="button h-full bg-red-600"
+          @click="inputs.reset"
+        >
+          clear
+        </button>
+        <button class="button h-full">
+          search
+        </button>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/components/find-ride/find-ride-result.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+// import { useFindRideStore } from '~/stores/findRideStore'
+
+// const inputs = useFindRideStore()
+</script>
+
+<template>
+  <h1>This is the result!</h1>
+</template>
