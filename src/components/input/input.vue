c4e64a187188b19ce33c161e4a6911bd9129bcba
Arm <69001162+arskiir@users.noreply.github.com>
2021-11-13 23:03:48 +0700

    ✨ Input component

---
+++ src/components/input/input.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string
+  setter: (value: any) => void
+  placeholder?: string
+}>()
+
+const value = ref('')
+watch(value, () => {
+  props.setter(value.value)
+})
+</script>
+
+<template>
+  <div class="flex flex-col w-full">
+    <span class="text-xl">{{ props.label }}</span>
+    <input
+      v-model="value"
+      type="text"
+      class="bg-transparent
+      border rounded p-3 mt-3
+      text-gray-100"
+      :placeholder="props.placeholder ?? props.label"
+    >
+  </div>
+</template>
