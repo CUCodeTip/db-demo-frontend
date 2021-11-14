2c15d4930e8d587e68bb5e9b4b35aca76edde7d2
Arm <69001162+arskiir@users.noreply.github.com>
2021-11-14 15:25:54 +0700

    ✨ first version of chat room page

---
+++ src/components/chat/chat-cover.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ChatCover } from '~/dumbChats'
+import { formatTime } from '~/utils'
+defineProps<{
+  chat: ChatCover
+}>()
+</script>
+
+<template>
+  <div class="flex px-3 space-x-10">
+    <div class="inline-block rounded-full w-25 h-25 overflow-hidden">
+      <img :src="chat.profile" class="object-cover object-center">
+    </div>
+    <div class="flex-grow space-y-3">
+      <div class="flex items-center">
+        <h4 class="font-semibold text-2xl flex-grow">
+          {{ chat.driver }}'s Ride
+        </h4>
+        <div class="flex items-center justify-center gap-3">
+          <formatted-date :date="chat.startDate" />
+          <formatted-time :date="chat.startDate" />
+          <passengers-count
+            :max-passengers="chat.maxPassengers"
+            :passengers="chat.passengers"
+          />
+        </div>
+      </div>
+      <div class="flex opacity-60 justify-between">
+        <p class="w-250">
+          {{ chat.latestMessage.text }}
+        </p>
+        <span>
+          {{ formatTime(chat.latestMessage.timestamp) }}
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
--- src/pages/chat.vue
+++ src/pages/chat.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
-
+import dumbChats from '~/dumbChats'
 </script>
 
 <template>
-  <h2>This is the chat page</h2>
+  <ol
+    class="overflow-y-scroll h-45rem
+    pr-3 space-y-5"
+  >
+    <li
+      v-for="(chat, idx) in dumbChats"
+      :key="idx"
+      class="bg-gray-700 rounded-xl p-4"
+    >
+      <chat-cover :chat="chat" />
+    </li>
+  </ol>
 </template>
