50a993e5864bc747ed348e3e229d974c10bcaa6c
Arm <69001162+arskiir@users.noreply.github.com>
2021-11-14 12:11:00 +0700

    ✨ ride-card comp

---
+++ src/components/ride/ride-card.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { RideStatus } from '~/types'
+
+defineProps<{
+  driver: string
+  maxPassengers: number
+  passengers: number
+  dateTime: Date
+  from: string
+  to: string
+  status: RideStatus
+}>()
+</script>
+
+<template>
+  <div class="flex">
+    <div class="flex-grow space-y-3">
+      <div>
+        <span class="hello-there">
+          <eva:pin-fill />
+          <span>From: {{ from }}</span>
+        </span>
+        <span class="hello-there">
+          <eva:pin-fill />
+          <span>To: {{ to }}</span>
+        </span>
+      </div>
+      <div class="flex space-x-3">
+        <formatted-date :date="dateTime" />
+        <formatted-time :date="dateTime" />
+        <passengers-count
+          :passengers="passengers"
+          :max-passengers="maxPassengers"
+        />
+      </div>
+    </div>
+    <div>
+      <ride-status :status="status" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.hello-there {
+  @apply block flex space-x-2 items-center text-xl;
+}
+</style>
