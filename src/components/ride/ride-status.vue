6ec0b5642b128cfc04bb39fabf8a72de021f46db
Arm <69001162+arskiir@users.noreply.github.com>
2021-11-14 11:52:23 +0700

    🚧 ride status in various colors

---
+++ src/components/ride/ride-status.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { RideStatus } from '~/types'
+
+const props = defineProps<{
+  status: RideStatus
+}>()
+
+const rideStatusToClass: {
+  [key in RideStatus]: string
+} = {
+  Booked: 'text-green-400',
+  Available: 'text-green-300',
+  Cancelled: 'text-#FF5353',
+  Ended: 'text-dark-900',
+  Full: 'text-orange-400',
+  Ongoing: 'text-blue-400',
+}
+const color = rideStatusToClass[props.status]
+</script>
+
+<template>
+  <span :class="[color, 'text-xl']">
+    {{ props.status }}
+  </span>
+</template>
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,6 @@
 import { ViteSSGContext } from 'vite-ssg'
 
 export type UserModule = (ctx: ViteSSGContext) => void
+
+export type RideStatus = 'Booked' | 'Available' | 'Full' |
+'Ongoing' | 'Ended' | 'Cancelled'
