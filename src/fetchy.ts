734d6184a84a2e69996b29b90abf3762722233d5
Arm <69001162+arskiir@users.noreply.github.com>
2021-11-19 21:45:19 +0700

    👽️ prefix api call with 'api'

--- src/fetchy.ts
+++ src/fetchy.ts
@@ -4,7 +4,7 @@ import { createFetch } from '@vueuse/core'
  * A custom useFetch instance with the base url, https://dumb-db-api.herokuapp.com/
  */
 const fetchy = createFetch({
-  baseUrl: 'https://dumb-db-api.herokuapp.com/',
+  baseUrl: 'https://dumb-db-api.herokuapp.com/api/',
   fetchOptions: {
     mode: 'cors',
   },
