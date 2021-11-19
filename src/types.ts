44f7894904de943064ddd1f1883ec35efce11947
Arm <69001162+arskiir@users.noreply.github.com>
2021-11-19 18:38:48 +0700

    📝 updated chat and message type conforming with mongo

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,20 @@ export type UserModule = (ctx: ViteSSGContext) => void
 // 'RideStatus' doesn't work bruh, idk why 🙄
 export type RideStatusBruh = 'Booked' | 'Available' | 'Full' |
 'Ongoing' | 'Ended' | 'Cancelled'
+
+export type Message = {
+  senderId: string
+  senderName: string
+  message: string
+  _id: string
+  createAt: Date
+}
+
+export type ChatCoverCard = {
+  _id: string
+  title: string
+  messages: Message[]
+  createdAt: Date
+  updatedAt: Date
+  __v: number
+}
