a27092945017f4be27f1abeb0e2923b7d86f699f
Arm <69001162+arskiir@users.noreply.github.com>
2021-11-21 00:34:02 +0700

    💄 added sidebar animation (consumes ungodly amount of power)

--- src/components/nav/nav.vue
+++ src/components/nav/nav.vue
@@ -12,6 +12,7 @@ const uStore = useUserStore()
   bg-gray-700 w-1/3
   rounded-tr-3rem rounded-br-3rem
   transition-all duration-1000 ease-in-out
+  overflow-hidden
   "
     :class="{
       'w-1/5': uStore.loggedInUser,
@@ -22,9 +23,7 @@ const uStore = useUserStore()
       <nav-login />
       <span class="mt-6 block opacity-60">CU CodeTip</span>
     </div>
+    <sparks class="-z-1" />
+    <wave class="-z-1" />
   </nav>
 </template>
-
-<style>
-
-</style>
