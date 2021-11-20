de86a572be0633e50d7422d2c0559606d1b0d810
Arm <69001162+arskiir@users.noreply.github.com>
2021-11-20 11:43:20 +0700

    ✨ redirect to '/' if there's no logged in user

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
-import { setMetadata } from './composables';
+import { setMetadata } from './composables'
+import { useUserStore } from './stores/user'
+
 setMetadata()
+
+const router = useRouter()
+const uStore = useUserStore()
+
+onBeforeMount(() => {
+  if (!uStore.loggedInUser)
+    router.replace('/')
+})
 </script>
 
 <template>
